fd03c3f2d69011725b13b11bb0827260bd334269
Oliver Micke <olivermicke@outlook.com>
2020-05-05 08:11:03 +0200

    feat: compatibility checker

---
+++ README.md
@@ -0,0 +1,14 @@
+## Blood Donation Compatibility Checker
+
+#### Output
+
+    == Recipient ==
+    John Doe [AB-]
+
+    == Donors ==
+    Jane Doe [A+]
+    Jürgen Doe [O-]
+
+    == Results ==
+    Jane Doe [A+] can't donate to John Doe [AB-]
+    Jürgen Doe [O-] can donate to John Doe [AB-]
---
+++ blood-donation-compability-checker.hs
@@ -0,0 +1,67 @@
+data Patient = Patient { name :: Name
+                       , bloodType:: BloodType }
+instance Show Patient where
+  show (Patient (firstName, lastName) bloodType) =
+      firstName ++ " " ++ lastName ++ " " ++
+      "[" ++ show bloodType ++ "]"
+
+data BloodType = BloodType { aboType :: ABOType
+                           , rhesusType :: RhesusType }
+instance Show BloodType where
+  show (BloodType aboType rhesusType) = show aboType ++ show rhesusType
+
+data ABOType = A | B | AB | O deriving(Show)
+data RhesusType = Positive | Negative
+instance Show RhesusType where
+  show Positive = "+"
+  show Negative = "-"
+
+type Donor = Patient
+type Recipient = Patient
+type DonorBloodType = BloodType
+type RecipientBloodType = BloodType
+type Name = (String, String)
+
+canDonateTo :: Donor -> Recipient -> Bool
+canDonateTo donor recipient = canDonateTo' (bloodType donor) (bloodType recipient)
+
+canDonateTo' :: DonorBloodType -> RecipientBloodType -> Bool
+canDonateTo' (BloodType _ Positive) (BloodType _ Negative) = False
+canDonateTo' (BloodType A _)        (BloodType A _)        = True
+canDonateTo' (BloodType A _)        (BloodType AB _)       = True
+canDonateTo' (BloodType A _)        _                      = False
+canDonateTo' (BloodType B _)        (BloodType B _)        = True
+canDonateTo' (BloodType B _)        (BloodType AB _)       = True
+canDonateTo' (BloodType B _)         _                     = False
+canDonateTo' (BloodType AB _)       (BloodType AB _)       = True
+canDonateTo' (BloodType AB _)       _                      = False
+canDonateTo' (BloodType O _)        _                      = True
+
+showCanDonateTo :: Donor -> Recipient -> String
+showCanDonateTo donor recipient = show donor ++ " " ++ canDonateStr ++ " " ++ show recipient
+  where canDonate = canDonateTo donor recipient
+        canDonateStr = (if canDonate then "can" else "can't") ++ " donate to"
+
+main :: IO ()
+main = do
+  let john = Patient { name = ("John", "Doe")
+                     , bloodType = BloodType AB Negative }
+
+  let jane = Patient { name = ("Jane", "Doe")
+                     , bloodType = BloodType A Positive }
+
+  let jürgen = Patient { name = ("Jürgen", "Doe")
+                       , bloodType = BloodType O Negative }
+
+  let showCanDonateToJohn donor = showCanDonateTo donor john
+
+  putStrLn "== Recipient =="
+  putStrLn $ show john
+  putStrLn ""
+  putStrLn "== Donors =="
+  putStrLn $ show jane
+  putStrLn $ show jürgen
+  putStrLn ""
+  putStrLn "== Results =="
+  putStrLn $ showCanDonateToJohn jane
+  putStrLn $ showCanDonateToJohn jürgen
